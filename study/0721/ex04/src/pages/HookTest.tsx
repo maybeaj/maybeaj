1e7b556ecf0a18dda5ca480e8ad403e41995c78a
이효진 <rabbit3456@naver.com>
2025-07-21 16:37:36 +0900

    chore: 컴포넌트 추가

--- study/0721/ex04/src/pages/HookTest.tsx
+++ study/0721/ex04/src/pages/HookTest.tsx
@@ -1,16 +1,17 @@
 import LayoutEffectExample from "../components/LayoutEffectExample";
 import IdExample from "../components/IdExample";
 import TransitionExample from "../components/TransitionExample";
+import ParentComponent from "../components/ParentComponent";
 
 function HookTest() {
 	return (
 		<div>
 			<LayoutEffectExample />
 			<IdExample />
-            <TransitionExample />
+			<TransitionExample />
+			<ParentComponent />
 		</div>
 	);
 }
 
 export default HookTest;
-
