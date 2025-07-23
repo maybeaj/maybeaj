7f942e9befeb606d2de0b8a21eac6bea8a2619e4
maybeaj <112530022+maybeaj@users.noreply.github.com>
2025-07-23 10:41:42 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    let arr = [...my_string];
+    let temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;   
+    let answer = arr.join('');
+    return answer;
+}
