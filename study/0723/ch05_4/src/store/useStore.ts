1b45260241a3d0c220639a2f489764d63f1920d6
이효진 <rabbit3456@naver.com>
2025-07-23 17:33:57 +0900

    0723: ch05_4 실습

--- study/0723/ch05_4/src/store/useStore.ts
+++ study/0723/ch05_4/src/store/useStore.ts
@@ -1,19 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {useMemo} from 'react'
-import {rootReducer} from './rootReducer'
+import { configureStore } from '@reduxjs/toolkit'
+import { useMemo } from 'react'
+import { rootReducer } from './rootReducer'
 import logger from 'redux-logger'
-import {thunk} from 'redux-thunk'
 
+// 개발 환경에서만 logger 활성화
 const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializeStore = () => {
-  const middleware: any[] = [thunk]
-  if (useLogger) {
-    middleware.push(logger)
-  }
   const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(middleware)
+    middleware: getDefaultMiddleware =>
+      useLogger
+        ? getDefaultMiddleware().concat(logger)
+        : getDefaultMiddleware()
   })
   return store
 }
